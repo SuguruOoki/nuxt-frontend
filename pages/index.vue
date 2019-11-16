603f89820ec1c15d16b9fb7ce60265d5a38ec9d9
SuguruOoki <oki.work0527@gmail.com>
2019-11-17 00:44:36 +0900

    Add pages

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+  </main>
+</template>
+
+<script>
+
+export default {
+  name: 'Default',
+  layout: 'Default',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss"></style>
